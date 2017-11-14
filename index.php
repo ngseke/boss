2bf0cef1d6d6dd2f783600c1fef7c897b6cb62c6
Huang Xingqiao <a92304a92304@gmail.com>
2017-11-14 11:43:58 +0800

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>BOSS</title>
+</head>
+<body>
+
+</body>
+</html>
