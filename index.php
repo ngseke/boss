735a564dc1cec5c019571057f19afedce272b317
Huang Xingqiao <a92304a92304@gmail.com>
2017-11-14 11:54:19 +0800

    新增bs css檔案

--- index.php
+++ index.php
@@ -8,9 +8,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="bootstrap.min.css">
   <title>BOSS</title>
 </head>
 <body>
-
+  <div class="container">
+    <div class="alert alert-primary text-center" role="alert">
+      歡迎光臨BOSS
+    </div>
+    <div class="alert alert-secondary text-center" role="alert">
+      這裡什麼也還沒有
+    </div>
+  </div>
 </body>
 </html>
