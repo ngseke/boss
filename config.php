09fc0e80c57b9a401e48e2e0d2ff4cd35b86b59e
Huang Xingqiao <a92304a92304@gmail.com>
2017-11-24 23:02:50 +0800

    修正資料庫名稱(Shop->BOSS)

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ if($_SERVER['HTTP_HOST'] == 'localhost'){
   define('db_host',   'localhost', false); // 資料庫host
   define('db_username', 'root', false);    // 資料庫用戶名
   define('db_password', '', false);        // 資料庫密碼
-  define('db_name',   'Shop', false);      // 資料庫名稱
+  define('db_name',   'BOSS', false);      // 資料庫名稱
  }else{
   // 若當前主機在遠端上
   define('db_host',   '', false);
