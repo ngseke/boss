70d19a704ea8ce05349398c88f96e7109b5e426e
Huang Xingqiao <a92304a92304@gmail.com>
2017-11-27 02:52:29 +0800

    修改index內容。

--- page.php
+++ page.php
@@ -1,10 +1,3 @@
 <?php
 $bg_url='https://c.pxhere.com/photos/05/e4/bar_human_bottles_beverages_alcohol_lamps_club_party-1333424.jpg!d' ;
 ?>
-<div class="jumbotron text-center bg-dark text-white" style="background:url('<?php echo $bg_url ?>');background-size: cover; background-position:center center; background-attachment:fixedX;">
-  <h1 class="display-3 my-0" style="opacity: .9">BOSS</h1>
-  <p class="lead "  style="opacity: .9">Beverage Online Shop System</p>
-  <h5 style="letter-spacing:1rem">現已盛大開幕</h5>
-</div>
-
-<?php echo md5('customer'); ?>
