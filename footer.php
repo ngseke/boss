1d8b24e8d4e03279cf0d80cfae6a1dde44f6dac7
Huang Xingqiao <a92304a92304@gmail.com>
2017-11-26 20:31:25 +0800

    加入簡易footer

--- footer.php
+++ footer.php
@@ -1,1 +1,11 @@
 <!-- 放footer的地方 -->
+
+<div class="container mt-3">
+  <div class="row">
+    <div class="col-12">
+      <small>
+        <p class="text-muted text-center">© Copyright BOSS</p>
+      </small>
+    </div>
+  </div>
+</div>
--- nav.php
+++ nav.php
@@ -8,7 +8,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
   <div class="container">
-    <a class="navbar-brand" href="index.php">BOSS</a>
+    <a class="navbar-brand" href="index.php">BOSS茶店</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
