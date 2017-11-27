b7ca6dcf206058460de0641d507b3fd25748f9c2
Huang Xingqiao <a92304a92304@gmail.com>
2017-11-27 16:36:48 +0800

    修改product_list架構。說明在readme.php。

--- readme.php
+++ readme.php
@@ -16,9 +16,20 @@
   商品 [搜尋列] 管理訂單 管理商品 管理會員(可以管S C A) 登出
 */
 
-
 // 1秒後自動跳轉
 <meta http-equiv="refresh" content="1;url=index.php" />
 
 // https://pxhere.com/
 // 背景圖素材
+
+
+/*
+
+  管理商品( product_list_*.php )
+    product_list                 列出所有商品(後台)
+      product_list_detail        該商品的詳情，顯示在input裡面，按下button可以送出修改
+        product_list_detail_edit (UPDATE)
+      product_list_new           INSERT新商品，提供input和上傳檔案欄位
+        product_list_new_add     (INSERT)
+
+*/
