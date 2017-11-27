f50e35b26ee374ebdc834a0ff2db26db5abd0d4a
ChaoChengTing <t104590036@ntut.org.tw>
2017-11-27 15:53:49 +0800

    垃圾桶按鈕+價格文字

--- cart.php
+++ cart.php
@@ -17,7 +17,7 @@
   <div class="container my-3">
 
     <div class="row" >
-      <div class="col-12 text-center";>
+      <div class="col-12 text-center ">
         <table class="table table-bordered">
           <thead class="thead-dark">
             <tr>
@@ -36,16 +36,19 @@
               <td class="align-middle">25</td>
               <td class="align-middle">1</td>
               <td class="align-middle">25</td>
-              <td class="align-middle"><i class="material-icons">delete</i></td>
+              <td class="align-middle">
+                <button type="button" class="btn btn-outline-dark"><i class="material-icons">delete</i></button>
+
+              </td>
             </tr>
             <tr>
               <td colspan="5">您尚未選購產品</td>
             </tr>
             <tr class="text-right">
               <td colspan="5">
-                共　件商品　商品金額：</br>
-                運費小計：</br>
-                <font size="+2">總金額：</font>
+                共<strong>1</strong>件商品　商品金額：NT$ <strong>123</strong></br>
+                運費小計：NT$ <strong>123</strong></br>
+                <font size="+2">總金額：NT$ <strong>123</strong></font>
               </td>
             </tr>
           </tbody>
