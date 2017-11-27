6e611dcbf3db1d54dfd889fc2c1d667365814785
ChaoChengTing <t104590036@ntut.org.tw>
2017-11-27 14:49:22 +0800

    購物車外觀

--- cart.php
+++ cart.php
@@ -30,13 +30,32 @@
           </thead>
           <tbody>
             <tr>
-              <th scope="row"><img src="http://www.pecos.com.tw/tmp/image/20140409/20140409202153_39623.jpg" alt="">
-                純喫茶綠茶</br>
+              <th scope="row" class="text-left"><img src="http://www.pecos.com.tw/tmp/image/20140409/20140409202153_39623.jpg" alt="">
+                純喫茶綠茶
               </th>
               <td>25</td>
               <td>1</td>
-              <td>1</td>
-              <td><img src="https://image.freepik.com/free-icon/trash-can_318-81009.jpg" height="25"></td>
+              <td>
+                <!--<form class="text-center"><input type="int" size="1" value="1"> </form>-->
+                <select multiple="multiple" size="1">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </td>
+              <td><img src="https://image.freepik.com/free-icon/trash-can_318-81009.jpg" height="25"><?php?>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="5">您尚未選購產品</td>
+            </tr>
+            <tr class="text-right">
+              <td colspan="5">
+                共　件商品　商品金額：</br>
+                運費小計：</br>
+                <font size="+2">總金額：</font>
+              </td>
             </tr>
           </tbody>
         </table>
