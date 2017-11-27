55cbe23442d1c155aa6006678e34ebf9a17fff70
吳品頤 <t104590020@ntut.org.tw>
2017-11-27 17:19:48 +0800

    add post comment

---
+++ post_comment.php
@@ -0,0 +1,47 @@
+<?php session_start(); ?>
+<?php include('connection.php'); ?>
+<?php $page_name = '所有商品' ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- 引入CSS等樣式內容 -->
+  <?php include('style.php') ?>
+  <title><?php echo  $page_name. ' - ' .title_name ?></title>
+</head>
+
+<body>
+  <!-- 引入導覽列 -->
+  <?php include('nav.php') ?>
+  <div class="container">
+    <div class="row">
+      <?php
+      //設定地點為台北時區
+        date_default_timezone_set('Asia/Taipei');
+      $CID=  $user_id;
+      $PID= $_POST['PID'];
+      $Date = date("Y/m/d H:i:s");//取得年份/月/日 時:分:秒
+      $Comment= $_POST['Comment'];
+      $Star=3;
+      $sql="INSERT INTO COMMENT(CID,PID,Star,Date,Comment)
+      VALUE('$CID',$PID,'$Star','$Date','$Comment')" ;
+      echo $sql;
+      if ($conn->query($sql)===true) {
+        echo '成功';
+      } else {
+        echo $conn->error . "<br>";
+      }
+      ?>
+
+    </div>
+  </div>
+  <?php include('footer.php') ?>
+</body>
+<!-- 引入JS -->
+<?php include('js.php') ?>
+
+</html>
