f27d4546c1cec01046bd69f8a4dc25794d9145ea
mdyu1000 <mdyu1000@gmail.com>
2017-11-29 13:23:29 +0800

    管理商品的頁面

---
+++ product_list.php
@@ -0,0 +1,25 @@
+<?php session_start(); ?>
+<?php include('connection.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- 引入CSS等樣式內容 -->
+  <?php include('style.php') ?>
+
+  <!-- 根據所在頁面 印出對應的標題 -->
+  <title><?php echo  $page_name. ' - ' .title_name ?></title>
+</head>
+<body>
+<?php include('nav.php'); ?>
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+
+    </div>
+  </div>
+</div>
+
+</body>
