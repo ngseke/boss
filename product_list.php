ae2b6d3f0e6608d1866cb1e608cbc8907f3e9324
mdyu1000 <mdyu1000@gmail.com>
2017-11-29 14:01:03 +0800

    新增表格 調整表格寬度 新增改爆按鈕

--- product_list.php
+++ product_list.php
@@ -17,6 +17,43 @@
 <div class="container">
   <div class="row">
     <div class="col-12">
+      <table class="table mt-5 d-none d-lg-table">
+        <thead>
+          <tr class="text-center">
+            <th scope="col">#</th>
+            <th scope="col">名稱</th>
+            <th scope="col">狀態</th>
+            <th scope="col">庫存數量</th>
+            <th scope="col">價錢</th>
+            <th scope="col">圖片</th>
+            <th scope="col">資訊</th>
+            <th scope="col">修改</th>
+          </tr>
+        </thead>
+        <tbody>
+          <form method="post" name="form1" action="XXXXXXX.php">
+            <?php
+            $sql = "SELECT * FROM product";
+            $result = $conn->query($sql);
+            if($result->num_rows > 0) {
+              while($row = $result->fetch_assoc()){
+                  echo
+                  '<tr>
+                  <td>' . $row["ID"] . '</td>
+                  <td>' . $row["Name"] . '</td>
+                  <td>' . $row["State"] . '</td>
+                  <td style="width:100px;">' . $row["Stock"] . '</td>
+                  <td style="width:75px;">' . $row["Price"] . '</td>';
+                  echo '<td> <img src ="' .$row['Img'] . '" height="100" width="100">';
+                  echo '</td>
+                  <td>' . $row["Info"] . '</td>
+                  <td> <button type="button" class="btn btn-primary">改爆</button> </td>
+                </tr>';
+            }
+          }
+          ?>
+        </tbody>
+      </table>
 
     </div>
   </div>
