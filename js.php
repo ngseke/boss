918a9471a1987ae7c09abfe359b53dd44f3e24e5
Huang Xingqiao <a92304a92304@gmail.com>
2017-12-01 20:36:51 +0800

    將管理折扣的js移至js.php

--- js.php
+++ js.php
@@ -1,7 +1,8 @@
 <!-- Bootstrap JS -->
-
 <script src="js/jquery-3.2.1.slim.min.js"></script>
 <script src="js/popper.min.js" ></script>
 <script src="js/bootstrap.min.js"></script>
 
-<!-- （已經有JQ了，所以不需再加入） -->
+<!-- ‘新增折扣’和‘修改折扣’頁面的js
+根據所選的項目動態顯示對應的input或select欄位 -->
+<script src="js/discount_display.js"></script>
