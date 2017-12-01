1db279d0353f1089d8c74e8dccbc197bc30a8737
Huang Xingqiao <a92304a92304@gmail.com>
2017-12-01 22:34:18 +0800

    修改多處BUG。 使用js，令新增折扣的日期欄位預設值自動輸入為今天。 “修改折扣”和“修改用戶”的下拉式選單的欄位裡面的選項改用php生成，才可為select正確的設定預設值value。 修改discount_list的表格顯示方式，新增折扣的旁邊新增淺顯易懂的折扣說明。

--- js.php
+++ js.php
@@ -6,3 +6,6 @@
 <!-- ‘新增折扣’和‘修改折扣’頁面的js
 根據所選的項目動態顯示對應的input或select欄位 -->
 <script src="js/discount_display.js"></script>
+
+<!-- 使用GetTodayDate()可以取得今天日期。 -->
+<script src="js/get_date.js"></script>
--- js/discount_display.js
+++ js/discount_display.js
@@ -3,16 +3,18 @@ function SetDiscountDisplay() {
   if($("select[name='Type']").val()=='shipping'){
     $("input[name='Rate']").parent().addClass('d-none');
     $("select[name='EventType']").parent().addClass('d-none');
-    $('#TypeDescribe').html('運費：達到特定金額享訂單免運費。');
+    $("input[name='Requirement']").parent().parent().removeClass('d-none');
+    $('#TypeDescribe').html('運費：達特定金額享“訂單免運費”。');
   } else if($("select[name='Type']").val()=='seasoning'){
     $("input[name='Rate']").parent().removeClass('d-none');
     $("select[name='EventType']").parent().addClass('d-none');
-    $('#TypeDescribe').html('季節：達到特定金額享訂單打折。');
+    $("input[name='Requirement']").parent().parent().removeClass('d-none');
+    $('#TypeDescribe').html('季節：達特定金額享“訂單打折”。');
   } else if($("select[name='Type']").val()=='event'){
     $("input[name='Rate']").parent().removeClass('d-none');
     $("select[name='EventType']").parent().removeClass('d-none');
     $("select[name='Rate']").addClass('d-none');
-    $('#TypeDescribe').html('活動：特定商品買一送一或打折。');
+    $('#TypeDescribe').html('活動：特定商品買一送一(BOGO)或打折(Discount)。');
   }
 }
 
@@ -23,7 +25,7 @@ function SetEventTypeDisplay(){
       $("input[name='Requirement']").parent().parent().addClass('d-none');
     } else if($("select[name='EventType']").val()=='discount'){
       $("input[name='Rate']").parent().removeClass('d-none');
-      $("input[name='Requirement']").parent().parent().removeClass('d-none');
+      $("input[name='Requirement']").parent().parent().addClass('d-none');
     }
   }
 }
