51b10c2d46e5de002b9c10b96d17576d5d9b7ea6
吳品頤 <t104590020@ntut.org.tw>
2017-12-04 17:49:09 +0800

    add member information

---
+++ member_information.php
@@ -0,0 +1,111 @@
+<?php session_start(); ?>
+<?php include('connection.php'); ?>
+<?php $page_name = '註冊' ?>
+<?php $login = isset($_SESSION['ID']) ? true : false ; ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <?php include('style.php') ?>
+  <title><?php echo  $page_name. ' - ' .title_name ?></title>
+</head>
+
+<body>
+  <?php include('nav.php'); ?>
+  <?php
+        $sql = "SELECT * FROM MEMBER
+                WHERE ID = '$user_id'";
+        $result = $conn->query($sql);
+        $row = mysqli_fetch_array($result);
+        $ID=$row['ID'];
+        $Password=$row['Password'];
+        $Name=$row['Name'];
+        $Email=$row['Email'];
+        $Phone=$row['Phone'];
+        $RegDate=$row['RegDate'];
+        $Birth=$row['Birth'];
+        $Gender=$row['Gender'];
+        $Address=$row['Address'];
+        $Position=$row['Position'];
+       ?>
+  <div class="container my-3">
+    <div class="row">
+      <div class="col-12 col-lg-6 offset-lg-3">
+        <div class="card">
+          <div class="card-header text-center">修改資料</div>
+          <div class="card-body">
+            <?php include('echo_alert.php') ?>
+            <form class="row" action="member_information_edit.php" method="post" enctype="multipart/form-data" >
+              <div class="col-12 form-group">
+                <label for="">帳號<span class="text-info">*</span></label>
+                <input type="text" value="<?php echo $ID;?>" name="ID" class="form-control" readonly>
+              </div>
+              <div class="col-12 form-group">
+                <label for="">姓名<span class="text-info">*</span></label>
+                <input type="text" value="<?php echo $Name;?>" name="Name" placeholder="" class="form-control" required>
+              </div>
+              <div class="col-12 form-group">
+                <label for="">密碼<span class="text-info">*</span></label>
+                <input type="text" name="Password" placeholder = "若不修改請留白" class="form-control" >
+              </div>
+              <div class="col-12 form-group">
+                <label for="">E-mail<span class="text-info">*</span></label>
+                <input type="email" value="<?php echo $Email;?>" name="Email" maxlength="30" class="form-control" required>
+              </div>
+              <div class="col-12 col-lg-6 form-group">
+                <label for="">電話<span class="text-info">*</span></label>
+                <input type="text" name="Phone" value="<?php echo $Phone;?>" placeholder="0987654321" maxlength="10" class="form-control" required>
+              </div>
+              <div class="col-12 col-lg-6 form-group">
+                <label for="">生日<span class="text-info">*</span></label>
+                <input type="date" value="<?php echo $Birth;?>" name="Birth" placeholder="Birth" class="form-control" >
+              </div>
+              <div class="col-12 col-lg-6 form-group">
+                <label for="">性別<span class="text-info">*</span></label>
+                <select class="form-control" value="<?php echo $Gender;?>" name="Gender" required>
+                  <?php
+                    // 如果直接對select標籤預設value是不管用der
+                    // 所以改以PHP生成下拉式選單的選項，為了個別設置原來選項的selected
+                    $genderList= array('M','F','N');
+                    $genderTextList= array('男(M)','女(F)','未知(N)');
+                    foreach ($genderList as $key => $ppap) {
+                      $isThisSelected = ($Gender==$ppap)?'selected ':'';
+                      echo '<option value="'.$ppap.'" '. $isThisSelected .' >'.$genderTextList[$key].'</option>';
+                    }
+                  ?>
+                </select>
+              </div>
+              <div class="col-12 col-lg-6 form-group">
+                <label for="">職位<span class="text-info">*</span></label>
+                <select class="form-control" value="<?php echo $Position;?>" name="Position" required>
+                  <?php
+                    // 以PHP生成下拉式選單的選項，為了個別設置原來選項的selected
+                    $positionList= array('C','S','A');
+                    $positionTextList= array('顧客 (Customer)','員工 (Staff)','管理員 (Admin)');
+                    foreach ($positionList as $key => $ppap) {
+                      $isThisSelected = ($Position==$ppap)?'selected ':'';
+                      echo '<option value="'.$ppap.'" '. $isThisSelected .' >'.$positionTextList[$key].'</option>';
+                    }
+                  ?>
+                </select>
+              </div>
+              <div class="col-12 form-group">
+                <label for="">地址<span class="text-info">*</span></label>
+                <input type="text" name="Address" value="<?php echo $Address;?>" maxlength="100" class="form-control" >
+              </div>
+              <div class="col-12 form-group">
+                <button class="btn btn-success btn-block" type="submit" >立即修改</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <?php include('footer.php') ?>
+</body>
+<?php include('js.php') ?>
+</html>
---
+++ member_information_edit.php
@@ -0,0 +1,52 @@
+<?php session_start(); ?>
+<?php include('connection.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <?php include('style.php') ?>
+  <meta http-equiv="refresh" content="<?php echo 0 ?>;URL=index.php">
+</head>
+
+<body>
+  <?php include('nav.php'); ?>
+  <div class="container my-3">
+
+    <div class="row">
+      <div class="col-12 text-center">
+        <?php
+        
+        //設定地點為台北時區
+        date_default_timezone_set('Asia/Taipei');
+
+        $ID=$_POST['ID'];
+        $Password=md5($_POST['Password']);
+        $Name=$_POST['Name'];
+        $Email=$_POST['Email'];
+        $Phone=$_POST['Phone'];
+        $Regdate=date("Y/m/d");
+        $Birth=$_POST['Birth'];
+        $Gender=$_POST['Gender'];
+        $Address=$_POST['Address'];
+        $Position=$_POST['Position'];
+
+        $sql= "UPDATE MEMBER
+        SET Password='$Password',Email='$Email',Name='$Name',Phone='$Phone',Regdate='$Regdate',Birth='$Birth',Gender='$Gender',Address='$Address',Position='$Position'
+        WHERE ID='$ID'";
+        $conn->query($sql);
+        
+        $_SESSION['AlertMsg'] =
+          array('success','<i class="material-icons">done</i> 修改用戶成功',false);
+        
+        $conn->close();
+        ?>
+      </div>
+    </div>
+
+  </div>
+  <?php include('footer.php') ?>
+</body>
+<?php include('js.php') ?>
+</html>
