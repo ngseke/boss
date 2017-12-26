4545bbc94f4db225598a8d604daf3dd1afabd9d3
Huang Xingqiao <a92304a92304@gmail.com>
2017-12-27 02:21:50 +0800

    優化一鍵查詢功能

--- db.php
+++ db.php
@@ -33,25 +33,52 @@ $arr = explode(';', $file);
 
 <body>
   <?php include('nav.php') ?>
-  <div class="container">
-    <div class="alert alert-primary text-center"><i class="material-icons">storage</i> 執行了以下查詢</div>
+  <div class="container mt-3">
+    <div class="alert alert-info text-center"><i class="material-icons">storage</i> 執行了以下查詢 <a data-toggle="collapse" href="#zhiling" class="alert-link">顯示</a></div>
+    <div class="" id="zhiling">
     <?php
       // 逐一執行mysql查詢
+      $errorNum=0;
       foreach ($arr as $line) {
+        ob_flush();
+        flush();
         if ($conn->query($line.';') == TRUE) {
           // 若正確
           if (!($rst = strpos($line, '--'))) { // 只印出非註解指令
-            echo '<div class="alert alert-secondary" ><pre><code>'. $line . ';</code></pre></div>';
+            echo '<div class="alert alert-secondary"><pre><code>'. $line . ';</code></pre></div>';
           }
         } else {
           // 若錯誤 印出錯誤資訊
-          echo'<div class="alert alert-danger" ><i class="material-icons">warning</i> 錯誤 :<br><pre><code>'
+          $errorNum++;
+          echo'<div class="alert alert-danger" id="error-query"><i class="material-icons">warning</i> 錯誤 :<br><pre><code>'
               . $line . ';<br><strong></code></pre>'. $conn->error .'</strong></div>';
+
         }
       }
     ?>
+    </div>
+    <?=($errorNum>0)
+     ?'<div class="alert alert-danger text-center" id="final-msg"><i class="material-icons">report_problem</i> 查詢時發生了共 '. $errorNum .' 筆錯誤！</div>'
+     :'<div class="alert alert-success text-center" id="final-msg"><i class="material-icons">favorite</i> 已成功查詢所有指令。</div>';
+     ?>
   </div>
   <?php include('footer.php') ?>
 </body>
 <?php include('js.php') ?>
+<script type="text/javascript">
+
+  // 頁面載入完成後，自動滾到最底部
+  $(document).ready(function(){
+    var h = $(document).height()-$(window).height();
+    $(document).scrollTop(h);
+
+    // 若查詢成功則將指令折疊。
+    if($('#final-msg').hasClass('alert-success')){
+      $('#zhiling').addClass('collapse');
+    }
+  });
+
+
+</script>
+
 </html>
