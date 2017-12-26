83fb83b92256fc5f70a85fe6355dd469f5102d12
吳品頤 <t104590020@ntut.org.tw>
2017-12-26 23:24:29 +0800

    xingqiao look this

--- customer_order.php
+++ customer_order.php
@@ -60,10 +60,15 @@ $page_name = '訂單';
 
       <?php 
       //變數宣告 是駝峰式的喔～
-      $orderNumber='';//訂單日期與編號
-      $orderProgessBar='';//訂單狀態列
+      $orderNumber = '';//訂單日期與編號
+      $orderProgressBar = '';//訂單狀態列
       $orderInformation = '';//收件者資訊
-      $orderDetail='';//訂單詳細內容
+      $orderDetail = '';//訂單詳細內容
+      $orderCost = '';//訂單總金額
+      $isSubmitted = '';
+      $isProcessed = '';
+      $isDelivered = '';
+      $isCompleted = '';
 
       //查詢全部的訂單
       $sql = "SELECT * FROM ORDER_LIST";
@@ -79,32 +84,113 @@ $page_name = '訂單';
 
 
       //印出資訊的方遜
-      function PrintOrder($orderNumber,$orderProgessBar,$orderInformation,$orderDetail) {
-        echo $orderNumber.$orderProgessBar.$orderInformation.$orderDetail;
+      function PrintOrder($orderNumber,$orderProgressBar,$orderInformation,$orderDetail,$orderCost) {
+        echo $orderNumber.$orderProgressBar.$orderInformation.$orderDetail.$orderCost;
       }
-
-      PrintOrder('1','2','3','4');
 
       if(isset($_GET['state'])){
         switch ($_GET['state']) {
           case 'all':
-            while($rowsAll = mysqli_fetch_array($result)){
+          while($rowsAll = mysqli_fetch_array($result)){
 
+            switch ($rowsAll['State']) {
+              case 'submitted':
+                $isSubmitted='bg-secondary';
+                $isProcessed='bg-primary';
+                $isDelivered='bg-primary';
+                $isCompleted='bg-primary';
+              break;
+              case 'processed':
+                $isSubmitted='bg-secondary';
+                $isProcessed='bg-secondary';
+                $isDelivered='bg-primary';
+                $isCompleted='bg-primary';
+              break;
+              case 'delivered':
+                $isSubmitted='bg-secondary';
+                $isProcessed='bg-secondary';
+                $isDelivered='bg-secondary';
+                $isCompleted='bg-primary';
+              break;
+              case 'completed':
+                $isSubmitted='bg-success';
+                $isProcessed='bg-success';
+                $isDelivered='bg-success';
+                $isCompleted='bg-success';
+              break; 
+              default:
+                  echo 'bitch!';
+              break;
             }
+
+
+            $orderNumber.='<div class="col-9"><div class="card mb-3"><h5 class="card-header">
+            <strong>'.$rowsAll['Date'].'</strong>訂單編號 : '.$rowsAll['ID'].'</h5>';
+            
+            $orderProgressBar .='<div class="card-body"><div class="row"><div class="col-12 mb-3">
+            <div class="progress" style="height: 2rem;">
+            <div class="progress-bar '.$isSubmitted.'text-light " style="width: 25%; ">已提交</div>
+            <div class="progress-bar '.$isProcessed.'text-light " style="width: 25%; ">處理中</div>
+            <div class="progress-bar '.$isDelivered.'text-light " style="width: 25%; ">運輸中</div>
+            <div class="progress-bar '.$isCompleted.'text-light " style="width: 25%; ">已完成</div>
+            </div></div>';
+
+            $orderInformation .='<div class="col-4"><strong>收件人:</strong>'.$user_name.'</div>
+              <div class="col-6"><strong>收件人電話:</strong> '.$user_phone.'</div>
+              <div class="col-6"><strong>收件地址:</strong> '.$user_address.'</div>'; 
+
+            $orderDetail .= ' <div class="col-12">
+                              <table class="table table-hover table-sm mt-3">
+                              <thead class="thead-dark">
+                              <tr>
+                                <th scope="col" style="width:2rem;">#</th>
+                                <th scope="col" style="width:4rem;"></th>
+                                <th scope="col">商品名稱</th>
+                                <th scope="col">單價</th>
+                                <th scope="col">數量</th>
+                              </tr>
+
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <th class="align-middle" scope="row">1</th>
+                                  <td class="text-center"><img src="http://www.pecos.com.tw/tmp/image/20140409/20140409202153_39623.jpg" class="img-fluid rounded d-block" style="max-height: 3rem; width: auto;"></td>
+                                  <td class="align-middle">純喫茶綠茶 </td>
+                                  <td class="align-middle">$ 199</td>
+                                  <td class="align-middle">99</td>
+                                </tr>
+                                <?php
+                                for ($i=0; $i < 5; $i++) {
+                                  echo '                        <tr>
+                                  <th class="align-middle" scope="row">1</th>
+                                  <td class="text-center"><img src="http://www.pecos.com.tw/tmp/image/20140409/20140409202153_39623.jpg" class="img-fluid rounded d-block" style="max-height: 3rem; width: auto;"></td>
+                                  <td class="align-middle">純喫茶綠茶 </td>
+                                  <td class="align-middle">$ 199</td>
+                                  <td class="align-middle">99</td>
+                                  </tr>';
+                                }
+                                ?>
+                              </tbody>
+                            </table>
+                          </div>';
+
+            PrintOrder($orderNumber,$orderProgressBar,$orderInformation,$orderDetail,$orderCost);
+
+          }
 
           break;
           case 'processed':
-            while($rowProcessed = mysqli_fetch_array($result)){
+          while($rowProcessed = mysqli_fetch_array($result)){
 
-            }
+          }
 
 
 
           break;
           case 'completed':
-            while($rowCompleted = mysqli_fetch_array($result)){
+          while($rowCompleted = mysqli_fetch_array($result)){
 
-            }
+          }
 
 
           break;
@@ -119,20 +205,11 @@ $page_name = '訂單';
       <div class="col-9">
         <div class="card mb-3">
           <h5 class="card-header">
-
-
-
             <strong>2017/12/12</strong>
             訂單編號 : ABCDEFGH
           </h5>
           <div class="card-body">
             <div class="row">
-              <div class="col-2 text-center d-none">
-                <h4><span class="badge badge-pill badge-secondary">已提交</span></h4>
-                <h4><span class="badge badge-pill badge-dark">處理中</span></h4>
-                <h4><span class="badge badge-pill badge-info">運輸中</span></h4>
-                <h4><span class="badge badge-pill badge-success">已完成</span></h4>
-              </div>
               <div class="col-12 mb-3">
                 <div class="progress" style="height: 2rem;">
                   <div class="progress-bar bg-primary text-light " style="width: 25%; ">已提交</div>
@@ -157,71 +234,6 @@ $page_name = '訂單';
                       <th scope="col">數量</th>
                     </tr>
 
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th class="align-middle" scope="row">1</th>
-                      <td class="text-center"><img src="http://www.pecos.com.tw/tmp/image/20140409/20140409202153_39623.jpg" class="img-fluid rounded d-block" style="max-height: 3rem; width: auto;"></td>
-                      <td class="align-middle">純喫茶綠茶 </td>
-                      <td class="align-middle">$ 199</td>
-                      <td class="align-middle">99</td>
-                    </tr>
-                    <?php
-                    for ($i=0; $i < 5; $i++) {
-                      echo '                        <tr>
-                      <th class="align-middle" scope="row">1</th>
-                      <td class="text-center"><img src="http://www.pecos.com.tw/tmp/image/20140409/20140409202153_39623.jpg" class="img-fluid rounded d-block" style="max-height: 3rem; width: auto;"></td>
-                      <td class="align-middle">純喫茶綠茶 </td>
-                      <td class="align-middle">$ 199</td>
-                      <td class="align-middle">99</td>
-                      </tr>';
-                    }
-                    ?>
-                  </tbody>
-                </table>
-              </div>
-              <div class="col-12 text-right">
-                總金額:  <h3 class="d-inline-block"><strong class="text-danger">NT$ 9990</strong></h3>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="card mb-3">
-          <h5 class="card-header">
-            <strong>2017/12/12</strong>
-            訂單編號 : ABCDEFGH
-          </h5>
-          <div class="card-body">
-            <div class="row">
-              <div class="col-2 text-center d-none">
-                <h4><span class="badge badge-pill badge-secondary">已提交</span></h4>
-                <h4><span class="badge badge-pill badge-dark">處理中</span></h4>
-                <h4><span class="badge badge-pill badge-info">運輸中</span></h4>
-                <h4><span class="badge badge-pill badge-success">已完成</span></h4>
-              </div>
-              <div class="col-12 mb-3">
-                <div class="progress" style="height: 2rem;">
-                  <div class="progress-bar bg-primary text-light " style="width: 25%; ">已提交</div>
-                  <div class="progress-bar bg-secondary text-light " style="width: 25%; ">處理中</div>
-                  <div class="progress-bar bg-secondary text-light " style="width: 25%; ">運輸中</div>
-                  <div class="progress-bar bg-secondary text-light " style="width: 25%; ">已完成</div>
-                </div>
-              </div>
-
-              <div class="col-4"><strong>收件人:</strong> 黃省喬</div>
-              <div class="col-6"><strong>收件人電話:</strong> 099999999</div>
-              <div class="col-6"><strong>收件地址:</strong> 台北市台北市台北市台北市台北市</div>
-
-              <div class="col-12">
-                <table class="table table-hover table-sm mt-3">
-                  <thead class="thead-dark">
-                    <tr>
-                      <th scope="col" style="width:2rem;">#</th>
-                      <th scope="col" style="width:4rem;"></th>
-                      <th scope="col">商品名稱</th>
-                      <th scope="col">單價</th>
-                      <th scope="col">數量</th>
-                    </tr>
                   </thead>
                   <tbody>
                     <tr>
