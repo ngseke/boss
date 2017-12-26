c2b95bbf0bf0161a0a03f9e94393cd480195d3c9
吳品頤 <t104590020@ntut.org.tw>
2017-12-27 00:03:05 +0800

    my loop is incorrect but i don't know why

--- customer_order.php
+++ customer_order.php
@@ -71,8 +71,19 @@ $page_name = '訂單';
       $isCompleted = '';
 
 
-      
 
+      function Initial(){
+        $orderNumber = '';
+        $orderProgressBar = '';
+        $orderInformation = '';
+        $orderDetail = '';
+        $orderCost = '';
+        $isSubmitted = '';
+        $isProcessed = '';
+        $isDelivered = '';
+        $isCompleted = '';
+        
+      }
 
       //印出資訊的方遜
       function PrintOrder($orderNumber,$orderProgressBar,$orderInformation,$orderDetail,$orderCost) {
@@ -85,27 +96,27 @@ $page_name = '訂單';
           //查詢全部的訂單
           $sql = "SELECT * FROM ORDER_LIST";
           $result = $conn->query($sql);
-          
+
           while($rowsAll = mysqli_fetch_array($result)){
 
             switch ($rowsAll['State']) {
               case 'submitted':
-              $isSubmitted='bg-secondary';
-              $isProcessed='bg-primary';
-              $isDelivered='bg-primary';
-              $isCompleted='bg-primary';
-              break;
-              case 'processed':
-              $isSubmitted='bg-secondary';
-              $isProcessed='bg-secondary';
-              $isDelivered='bg-primary';
-              $isCompleted='bg-primary';
-              break;
-              case 'delivered':
-              $isSubmitted='bg-secondary';
+              $isSubmitted='bg-primary';
               $isProcessed='bg-secondary';
               $isDelivered='bg-secondary';
-              $isCompleted='bg-primary';
+              $isCompleted='bg-secondary';
+              break;
+              case 'processed':
+              $isSubmitted='bg-primary';
+              $isProcessed='bg-primary';
+              $isDelivered='bg-secondary';
+              $isCompleted='bg-secondary';
+              break;
+              case 'delivered':
+              $isSubmitted='bg-primary';
+              $isProcessed='bg-primary';
+              $isDelivered='bg-primary';
+              $isCompleted='bg-secondary';
               break;
               case 'completed':
               $isSubmitted='bg-success';
@@ -124,10 +135,10 @@ $page_name = '訂單';
             
             $orderProgressBar .='<div class="card-body"><div class="row"><div class="col-12 mb-3">
             <div class="progress" style="height: 2rem;">
-            <div class="progress-bar '.$isSubmitted.'text-light " style="width: 25%; ">已提交</div>
-            <div class="progress-bar '.$isProcessed.'text-light " style="width: 25%; ">處理中</div>
-            <div class="progress-bar '.$isDelivered.'text-light " style="width: 25%; ">運輸中</div>
-            <div class="progress-bar '.$isCompleted.'text-light " style="width: 25%; ">已完成</div>
+            <div class="progress-bar '.$isSubmitted.' text-light " style="width: 25%; ">已提交</div>
+            <div class="progress-bar '.$isProcessed.' text-light " style="width: 25%; ">處理中</div>
+            <div class="progress-bar '.$isDelivered.' text-light " style="width: 25%; ">運輸中</div>
+            <div class="progress-bar '.$isCompleted.' text-light " style="width: 25%; ">已完成</div>
             </div></div>';
 
             $orderInformation .='<div class="col-4"><strong>收件人:</strong>'.$user_name.'</div>
@@ -154,23 +165,16 @@ $page_name = '訂單';
             <td class="align-middle">$ 199</td>
             <td class="align-middle">99</td>
             </tr>
-            <?php
-            for ($i=0; $i < 5; $i++) {
-              <tr>
-              <th class="align-middle" scope="row">1</th>
-              <td class="text-center"><img src="http://www.pecos.com.tw/tmp/image/20140409/20140409202153_39623.jpg" class="img-fluid rounded d-block" style="max-height: 3rem; width: auto;"></td>
-              <td class="align-middle">純喫茶綠茶 </td>
-              <td class="align-middle">$ 199</td>
-              <td class="align-middle">99</td>
-              </tr>;
-            }
-            ?>
             </tbody>
             </table>
             </div>';
 
-            PrintOrder($orderNumber,$orderProgressBar,$orderInformation,$orderDetail,$orderCost);
+            $orderCost .= '<div class="col-12 text-right">
+                總金額:  <h3 class="d-inline-block"><strong class="text-danger">NT$ '.$rowsAll['FinalCost'].'</strong></h3>
+              </div></div></div></div></div>';
 
+            PrintOrder($orderNumber,$orderProgressBar,$orderInformation,$orderDetail,$orderCost);
+            Initial();
           }
 
           break;
@@ -178,7 +182,7 @@ $page_name = '訂單';
           //查詢未完成的訂單
           $sql = "SELECT * FROM ORDER_LIST";
           $result = $conn->query($sql);
-          
+
           while($rowProcessed = mysqli_fetch_array($result)){
 
           }
@@ -199,75 +203,13 @@ $page_name = '訂單';
 
           break;
           default:
-              # code...
+              echo 'bitch';
           break;
         }
       }
 
       ?>
 
-      <div class="col-9">
-        <div class="card mb-3">
-          <h5 class="card-header">
-            <strong>2017/12/12</strong>
-            訂單編號 : ABCDEFGH
-          </h5>
-          <div class="card-body">
-            <div class="row">
-              <div class="col-12 mb-3">
-                <div class="progress" style="height: 2rem;">
-                  <div class="progress-bar bg-primary text-light " style="width: 25%; ">已提交</div>
-                  <div class="progress-bar bg-secondary text-light " style="width: 25%; ">處理中</div>
-                  <div class="progress-bar bg-secondary text-light " style="width: 25%; ">運輸中</div>
-                  <div class="progress-bar bg-secondary text-light " style="width: 25%; ">已完成</div>
-                </div>
-              </div>
-
-              <div class="col-4"><strong>收件人:</strong> 黃省喬</div>
-              <div class="col-6"><strong>收件人電話:</strong> 099999999</div>
-              <div class="col-6"><strong>收件地址:</strong> 台北市台北市台北市台北市台北市</div>
-
-              <div class="col-12">
-                <table class="table table-hover table-sm mt-3">
-                  <thead class="thead-dark">
-                    <tr>
-                      <th scope="col" style="width:2rem;">#</th>
-                      <th scope="col" style="width:4rem;"></th>
-                      <th scope="col">商品名稱</th>
-                      <th scope="col">單價</th>
-                      <th scope="col">數量</th>
-                    </tr>
-
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th class="align-middle" scope="row">1</th>
-                      <td class="text-center"><img src="http://www.pecos.com.tw/tmp/image/20140409/20140409202153_39623.jpg" class="img-fluid rounded d-block" style="max-height: 3rem; width: auto;"></td>
-                      <td class="align-middle">純喫茶綠茶 </td>
-                      <td class="align-middle">$ 199</td>
-                      <td class="align-middle">99</td>
-                    </tr>
-                    <?php
-                    for ($i=0; $i < 5; $i++) {
-                      echo '                        <tr>
-                      <th class="align-middle" scope="row">1</th>
-                      <td class="text-center"><img src="http://www.pecos.com.tw/tmp/image/20140409/20140409202153_39623.jpg" class="img-fluid rounded d-block" style="max-height: 3rem; width: auto;"></td>
-                      <td class="align-middle">純喫茶綠茶 </td>
-                      <td class="align-middle">$ 199</td>
-                      <td class="align-middle">99</td>
-                      </tr>';
-                    }
-                    ?>
-                  </tbody>
-                </table>
-              </div>
-              <div class="col-12 text-right">
-                總金額:  <h3 class="d-inline-block"><strong class="text-danger">NT$ 9990</strong></h3>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 
