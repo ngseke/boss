40b31de0f1b8aedb84658f45de49b5f0d75dd322
Huang Xingqiao <a92304a92304@gmail.com>
2018-01-26 13:13:16 +0800

    del sensitive information

--- config.php
+++ config.php
@@ -4,16 +4,14 @@
 
 if($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST']== '127.0.0.1'){
   // 若當前主機為localhost
-  define('db_host',    'localhost', false); // 資料庫host
+  define('db_host',     'localhost', false); // 資料庫host
   define('db_username', 'root',     false); // 資料庫用戶名
   define('db_password', '',         false); // 資料庫密碼
   define('db_name',     'BOSS',     false); // 資料庫名稱
  }else{
   // 若當前主機在遠端上
-  define('db_host',   'localhost', false);
-  define('db_username', 'id4146602_xingqiao', false);
-  define('db_password', 'xingqiao', false);
-  define('db_name',   'id4146602_boss', false);
+  // 建立 'host_info.php'，如上方格式定義資料庫資訊。
+  require_once 'host_info.php';
  }
 
 define('auto_jump_time', 1, false); // 自動跳轉的秒數
